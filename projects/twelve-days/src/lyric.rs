a26f23e2a5ca562718ec9cdc51356fe106818c8f
syaw0 <siamhb7@protonmail.com>
2024-07-30 14:17:26 +0330

    complete the twelve-days mini project

---
+++ projects/twelve-days/src/lyric.rs
@@ -0,0 +1,13 @@
+pub const LYRIC: [&str; 11] = [
+    "Two turtle doves,",
+    "Three French hens,",
+    "Four calling birds,",
+    "Five golden rings,",
+    "Six geese a-laying,",
+    "Seven swans a-swimming,",
+    "Eight maids a-milking,",
+    "Nine ladies dancing,",
+    "Ten lords a-leaping,",
+    "Eleven pipers piping,",
+    "Twelve drummers drumming,",
+];
--- projects/twelve-days/src/main.rs
+++ projects/twelve-days/src/main.rs
@@ -1,3 +1,20 @@
+mod lyric;
+
+// ======================================================================
+
 fn main() {
-    println!("Hello, world!");
+    let mut lyric_lines: Vec<String> = Vec::new();
+
+    println!("{}", print_lyric(&lyric_lines));
+    for section in lyric::LYRIC {
+        lyric_lines.insert(0, section.to_string());
+        println!("{}", print_lyric(&lyric_lines));
+    }
+}
+
+fn print_lyric(lyric_lines: &[String]) -> String {
+    let lyric_text = lyric_lines.join("\n");
+    return format!(
+        "On the first day of Christmas,\nmy true love gave to me\n{lyric_text}\nA partridge in a pear tree.\n"
+    );
 }
