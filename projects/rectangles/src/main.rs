319b4d1f09e8976d7cb6a7704e02340089a2a11c
syaw0 <siamhb7@protonmail.com>
2024-07-31 15:02:52 +0330

    complete the challange of rectangles with

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn rectangle_builder(width: u32, height: u32) -> Rectangle {
+    Rectangle {
+        height,
+        width,
+    }
+}
+
+fn main() {
+    // * Step 1:
+    // let width = 100;
+    // let height = 140;
+
+    // let area = rectangle_area(width, height);
+
+    // * Step 2:
+    // let rect1 = Rectangle { height: 140, width: 100 };
+    // let area = rect1.area();
+
+    // * Step 3:
+
+    let rect1 = rectangle_builder(100, 140);
+
+    let area = rect1.area();
+
+    // println!("{:#?}", rect1);
+    println!("the area of rectangle is : {area}");
+    dbg!(&rect1);
+}
+
+// fn rectangle_area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
